1219a377970ee552332d83a6d68183301237c859
Divyanshu Pandey <dvynshu.pndey@gmail.com>
2019-06-15 17:00:04 +0530

    refactored code

--- public/js/client.js
+++ public/js/client.js
@@ -1,35 +1,40 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+
     var socket_connect = function (room) {
         return io('localhost:3000', {
             query: 'r_var=' + room
         });
     }
 
+
     // Room id
     var room = document.getElementById('room').innerText.replace(/\s\s+/g, '');
 
-    console.log('client wla room ' + room);
-    console.log(' next');
 
     // Connect to room
     var socket = socket_connect(room);
 
+
     var mode = "pen";
+
 
     var pen = document.getElementById('pen');
     var eraser = document.getElementById('eraser');
+
 
     // Select pen
     pen.onclick = function () {
         mode = "pen";
     }
 
+
     // Select eraser
     eraser.onclick = function () {
         mode = "eraser";
     }
+
 
     // To track position of mouse
     var mouse = {
@@ -39,33 +44,41 @@ document.addEventListener("DOMContentLoaded", () => {
         pos_prev: false
     }
 
+
     // Get Canvas element and create context
     var canvas = document.getElementById('drawing');
     var context = canvas.getContext('2d');
     var width = window.innerWidth / 1.05;
     var height = window.innerHeight / 1.3;
 
+
     // set canvas to full browser width/height
     canvas.width = width;
     canvas.height = height;
+
 
     // register mouse event handlers
     canvas.onmousedown = function (e) {
         mouse.click = true;
     };
+
     canvas.onmouseup = function (e) {
         mouse.click = false;
     };
 
+
     canvas.onmousemove = function (e) {
+
         // normalize mouse position to range 0.0 - 1.0
         mouse.pos.x = e.clientX / width;
         mouse.pos.y = e.clientY / height * 0.85;
         mouse.move = true;
     };
 
+
     // draw line received from server
     socket.on('draw_line', function (data) {
+
         var line = data.line;
         context.beginPath();
         if (data.mode == "pen") {
@@ -76,14 +89,18 @@ document.addEventListener("DOMContentLoaded", () => {
             context.strokeStyle = "#FFFFFF";
             context.lineWidth = 5;
         }
+
         // context.globalCompositeOperation = "source-over";
         context.moveTo(line[0].x * width, line[0].y * height);
         context.lineTo(line[1].x * width, line[1].y * height);
         context.stroke();
+
     });
+
 
     // main loop, running every 25ms
     function mainLoop() {
+
         // check if the user is drawing
         if (mouse.click && mouse.move && mouse.pos_prev) {
             // send line to to the server
@@ -93,6 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
         mouse.pos_prev = { x: mouse.pos.x, y: mouse.pos.y };
         setTimeout(mainLoop, 25);
     }
+
     mainLoop();
 
 });
